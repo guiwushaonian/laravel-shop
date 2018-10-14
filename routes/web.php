ea575605468b4e0464fe173fa2a2192bf84ca12a
高旭辉 <519996283@qq.com>
2018-10-14 16:50:11 +0800

    修改和删除收货地址

--- routes/web.php
+++ routes/web.php
@@ -27,5 +27,12 @@ Route::group(['middleware' => 'auth'], function () {
         Route::get('user_addresses/create', 'UserAddressesController@create')->name('user_addresses.create');
         // 提交新增收货地址
         Route::post('user_addresses', 'UserAddressesController@store')->name('user_addresses.store');
+        // 编辑收货地址
+        Route::get('user_addresses/{user_address}', 'UserAddressesController@edit')->name('user_addresses.edit');
+        // 更新收货地址
+        Route::put('user_addresses/{user_address}', 'UserAddressesController@update')->name('user_addresses.update');
+        // 删除收货地址
+        Route::delete('user_addresses/{user_address}',
+            'UserAddressesController@destroy')->name('user_addresses.destroy');
     });
 });
